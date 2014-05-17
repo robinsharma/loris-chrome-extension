e152fcd1543748f0bae5376c1d826219a57f353d
Robin Sharma <sharms115@gmail.com>
2014-05-16 22:40:16 -0400

    Formatting

--- js/background.js
+++ js/background.js
@@ -1,8 +1,8 @@
 (function renderPageAction () {
-	chrome.tabs.onUpdated.addListener(showPageAction);
-	function showPageAction (tabId, data, tab) {
-		if (tabId) { //TODO check if on loris only
-			chrome.pageAction.show(tabId);
-		}
-	}
+    chrome.tabs.onUpdated.addListener(showPageAction);
+    function showPageAction (tabId, data, tab) {
+        if (tabId) { //TODO check if on loris only
+            chrome.pageAction.show(tabId);
+        }
+    }
 }(chrome));
--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 (function allowExport() {
-	//document.querySelector('.output-button').textContent = 'Tests';
+    //document.querySelector('.output-button').textContent = 'Tests';
 }(chrome)); // dependencies
